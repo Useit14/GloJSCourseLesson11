8fd890068453e13403068a42c32fee67400bd4cc
Useit14 <useit14@inbox.ru>
2022-08-12 12:33:00 +0300

    mission is complete

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const button = document.querySelector("button");
+const square = document.querySelector("#square");
+const inputText = document.querySelector("input[type=text]");
+const inputRange = document.querySelector("input[type=range]");
+const e_btn = document.querySelector("#e_btn");
 
+const handlerClick = () => {
+  square.style = `background:${inputText.value}`;
+};
 
+const handlerInput = (event) => {
+  const circle = document.querySelector("#circle");
+  circle.style = `width:${event.target.value}%;hight:${event.target.value}%`;
+};
 
+button.addEventListener("click", handlerClick);
+inputRange.addEventListener("input", handlerInput);
 
-
+e_btn.style = "display:none";
