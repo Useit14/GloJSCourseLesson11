83a6a731f8f5cae18cd518f213fb346d5b5adeaa
Useit14 <useit14@inbox.ru>
2022-08-13 13:10:18 +0300

    Fix problem

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const square = document.querySelector("#square");
 const inputText = document.querySelector("input[type=text]");
 const inputRange = document.querySelector("input[type=range]");
 const e_btn = document.querySelector("#e_btn");
+const span = document.querySelector("#range-span");
 
 const handlerClick = () => {
   square.style = `background:${inputText.value}`;
@@ -10,11 +11,12 @@ const handlerClick = () => {
 
 const handlerInput = (event) => {
   const circle = document.querySelector("#circle");
-  const span = document.querySelector("#range-span");
+
   span.textContent = event.target.value;
   circle.style = `width:${event.target.value}%;height:${event.target.value}%`;
 };
 
+span.textContent = inputRange.value;
 button.addEventListener("click", handlerClick);
 inputRange.addEventListener("input", handlerInput);
 
